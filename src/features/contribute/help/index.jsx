badc02ec63c9f8f20e79be5048cf14ded8a1e53c
Nicolas-Sy <nicolas_sy@dlsu.edu.ph>
2021-07-13 00:13:13 +0800

    fixed UI of Help Page

--- src/features/contribute/help/index.jsx
+++ src/features/contribute/help/index.jsx
@@ -18,7 +18,7 @@ export default function HelpDirectory() {
       </HelpItem>
       <HelpItem heading="Classifying Obstructions">
         <div>
-          <P className="text-xl mb-2"> We classify "obstructions" as objects that <b>block the pathway</b> for a pedestrian to easily walk along the sidewalk. </P>
+          <P className="mb-2"> We classify "<u>obstructions</u>" as objects that <b>block the pathway</b> for a pedestrian to easily walk along the sidewalk. </P>
           <P> 1. Click on a bounding box on the image.  </P>
           <P> 2. If the object blocks the pathway of the sidewalk, select "Yes" as your option, otherwise select "No". </P>
           <P> 3. The object will appear on the "Selected Obstructions" list if you selected "Yes". </P>
@@ -26,13 +26,13 @@ export default function HelpDirectory() {
       </HelpItem>
       <HelpItem heading="Labeling New Obstructions">
         <div>
-          <P className="text-xl mb-2">If you see an obstruction on the image that <b>does not have a bounding box around it yet</b>, it's time to label it as an obstruction!</P>
+          <P className="mb-2">If you see an obstruction on the image that <b>does not have a bounding box around it yet</b>, it's time to label it as an <u>obstruction</u>!</P>
           <P> 1. Point your mouse on the obstruction.</P>
           <P> 2. Click and drag to draw a bounding box around it. </P>
           <P> 3. Click the box (we can make this automatic na). </P>
           <P> 4. Select the type of object among the selection list. </P>
           <P> 5. Click the "check mark" icon to confirm your action, otherwise click the "trash bin" icon to delete your action. </P>
-          <P> 6. The new obstruction will appear on the "New Obstructions" list.</P>
+          <P className="mb-2"> 6. The new obstruction will appear on the "New Obstructions" list.</P>
           <P> <b> Note: </b>We have limited objects among the options, as these are the objects that can be commonly found along
             Philippine sidewalks. If the object that you are about to label is not on the list, please do not label it anymore. </P>
         </div>
@@ -40,8 +40,8 @@ export default function HelpDirectory() {
       <HelpItem heading="Rating Sidewalk Accessibility">
         <div>
           <P> 1. Place your mouse over the slider and drag it towards the accessibility rating of the sidewalk found in the image.</P>
-          <P> 2. An accessibility rating of 1 means that the sidewalk is completely unsafe and inaccessible for both abled pedestrians and PWPDs. </P>
-          <P> 3. An accessibility rating of 10 means that the sidewalk has no accessibility nor safety issues for both abled pedestrians and PWPDs.</P>
+          <P> 2. An accessibility rating of 1 means that the sidewalk is completely <b> unsafe and inaccessible </b> for both abled pedestrians and PWPDs. </P>
+          <P> 3. An accessibility rating of 10 means that the sidewalk is very <b> safe and accessible </b>for both abled pedestrians and PWPDs.</P>
         </div>
       </HelpItem>
       <HelpItem heading="Determining Surface Type">
